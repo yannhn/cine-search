89b2bad45f395c9e0e3a46ca63c18e503acc3e85
Yannik Hangst <yanso1@posteo.de>
2023-05-13 01:54:48 +0200

    setup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require("path");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.ts",
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
@@ -9,13 +20,18 @@ module.exports = {
         use: "ts-loader",
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, "src"),
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
     ],
   },
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
 };
