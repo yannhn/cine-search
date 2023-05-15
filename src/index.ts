44768361c89067a8c9ef6e873183fa0a3fb14c27
Yannik Hangst <yanso1@posteo.de>
2023-05-16 00:17:42 +0200

    delete console log

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ const api = new FetchAPI("https://api.themoviedb.org/3/", process.env.API_KEY);
 
 console.log(api.get("trending/movie/week").then((data) => console.log(data)));
 
-fetch(
-  `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.API_KEY}`
-)
-  .then((res: any) => res.json())
-  .then((data: any) => console.log(data));
-
 function init(callback: any) {
   if (document.readyState == "loading") {
     document.addEventListener("DOMContentLoaded", callback);
