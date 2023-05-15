5fa2576f2f3861ff58da14ccb2f85e099984b5f5
Yannik Hangst <yanso1@posteo.de>
2023-05-16 00:17:20 +0200

    fix fetch

--- src/components/fetchAPI.ts
+++ src/components/fetchAPI.ts
@@ -1,13 +1,17 @@
 export class FetchAPI {
   private baseUrl: string;
+  private API_KEY: string;
 
-  constructor(baseUrl: string) {
+  constructor(baseUrl: string, API_KEY: string) {
     this.baseUrl = baseUrl;
+    this.API_KEY = API_KEY;
   }
 
   async get(url: string) {
     try {
-      const response = await fetch(this.baseUrl + url);
+      const response = await fetch(
+        `${this.baseUrl}${url}?api_key=${this.API_KEY}`
+      );
       const data = await response.json();
       return data;
     } catch (err) {
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,20 @@
 import "./style.css";
 import { FetchAPI } from "./components/fetchAPI";
 
-const api = new FetchAPI("https://dog.ceo/api/breeds/image/");
+const api = new FetchAPI("https://api.themoviedb.org/3/", process.env.API_KEY);
 
-console.log(api.get("random").then((data) => console.log(data)));
+console.log(api.get("trending/movie/week").then((data) => console.log(data)));
 
-const main = () => {
-  console.log("console test");
-};
+fetch(
+  `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.API_KEY}`
+)
+  .then((res: any) => res.json())
+  .then((data: any) => console.log(data));
 
-main();
+function init(callback: any) {
+  if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", callback);
+  } else {
+    callback();
+  }
+}
