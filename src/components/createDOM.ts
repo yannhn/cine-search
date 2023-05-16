934bf08c7da4919068a5f89389094a4e0544226d
Yannik Hangst <yanso1@posteo.de>
2023-05-16 14:07:02 +0200

    render fetch results

---
+++ src/components/createDOM.ts
@@ -0,0 +1,32 @@
+export class CreateDOM {
+  private container: HTMLDivElement;
+
+  constructor() {
+    this.container = document.createElement("div");
+  }
+
+  createElement(
+    tagName: string,
+    attributes: Record<string, string> = {},
+    classes: string[] = [],
+    parentElement: HTMLElement | null = null
+  ) {
+    const element = document.createElement(tagName);
+
+    if (attributes !== undefined && typeof attributes === "object") {
+      for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+      }
+    }
+
+    if (classes !== undefined && Array.isArray(classes)) {
+      element.classList.add(...classes);
+    }
+
+    if (parentElement !== null) {
+      parentElement.appendChild(element);
+    }
+
+    return element;
+  }
+}
