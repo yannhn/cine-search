05e87ae6a728b77f38b9b6349b8f553d714202f3
Yannik Hangst <yanso1@posteo.de>
2023-06-03 17:11:09 +0200

    implement font awesome

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
 import "./style.css";
+import "@fortawesome/fontawesome-free/js/fontawesome";
+import "@fortawesome/fontawesome-free/js/solid";
+import "@fortawesome/fontawesome-free/js/regular";
+import "@fortawesome/fontawesome-free/js/brands";
+
 import { FetchAPI } from "./components/fetchAPI";
 import { CreateDOM } from "./components/createDOM";
 import {
